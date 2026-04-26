46e80aadb1bd4478202e702d9c457dbf621448cc
Minhal Sohail <l250824@lhr.nu.edu.pk>
2026-04-26 19:39:43 +0500

    User.cpp  class deffinations

---
+++ User.cpp
@@ -0,0 +1,100 @@
+#include "User.h"
+#include <iostream>
+using namespace std;
+
+// Default Constructor
+User::User()
+{
+    id = "";
+    name = "";
+    friendIDs = nullptr;
+    friendCount = 0;
+    likedPageIDs = nullptr;
+    likedPageCount = 0;
+}
+
+// Parameterized Constructor
+User::User(string id, string name)
+{
+    this->id = id;
+    this->name = name;
+    friendIDs = nullptr;
+    friendCount = 0;
+    likedPageIDs = nullptr;
+    likedPageCount = 0;
+}
+
+// Getters
+string User::getId() const
+{
+    return id;
+}
+
+string User::getName() const
+{
+    return name;
+}
+
+string* User::getFriendIDs() const
+{
+    return friendIDs;
+}
+
+int User::getFriendCount() const
+{
+    return friendCount;
+}
+
+string* User::getLikedPageIDs() const
+{
+    return likedPageIDs;
+}
+
+int User::getLikedPageCount() const
+{
+    return likedPageCount;
+}
+ // adding a friend
+void User::addFriend(string friendId)
+{
+    string* temp = new string[friendCount + 1]; // larger array of string type to store friend IDs
+    for (int i = 0; i < friendCount; i++)
+    {
+        temp[i] = friendIDs[i]; // copyingg to tem
+    }
+    temp[friendCount] = friendId; //adding new id
+
+    delete friendID;
+    friendID = temp; // putting everything in friend id array
+    friendCount++;
+}
+
+//adding a Liked Page
+void User::addLikedPage(string pageId)
+{
+    string* temp = new string[likedPageCount + 1]; // larger array of string type to store likedpage IDs
+    for (int i = 0; i < likedPageCount; i++)
+    {
+        temp[i] = likedPageIDs[i]; // copying to temp
+    }
+    temp[likedPageCount] = pageId;// adding new id
+
+    delete likedPageIDs;
+    likedPageIDs = temp; // putting everything in liked page ids aray
+    likedPageCount++;
+}
+
+// Display (Optional Helper)
+void User::display() const
+{
+    cout << id << " - " << name << endl;
+}
+
+// Destructor
+User::~User()
+{
+    delete[] friendIDs;
+    friendIDs = nullptr;
+    delete[] likedPageIDs;
+    likedPageIDs = nullptr;
+}
